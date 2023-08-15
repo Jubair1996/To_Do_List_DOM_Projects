6ae9e48b7fa6ebd95361b9497eb7d3f31564c7d4
Jubari1996 <abujubari1996@gmail.com>
2023-08-15 13:47:36 +0600

    Enter click added

--- todo.js
+++ todo.js
@@ -42,3 +42,52 @@ document.getElementById("input-btn").addEventListener("click", function () {
   });
 });
 
+// Enter click
+
+const input = document.getElementById("input-value");
+input.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+
+    count += 1;
+    const myInputValue = document.getElementById("input-value");
+    const myInputText = myInputValue.value;
+    myInputValue.value = "";
+
+    const parent = document.getElementById("content-container");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${count}</td>
+    <td>${myInputText}</td>
+    <td>
+    <button class="btn btn-danger deleteBtn">Delete</button>
+    <button class="btn btn-primary doneBtn">Done</button>
+    </td>
+    `;
+    parent.appendChild(tr);
+
+    // Delete
+
+    const elements = document.querySelectorAll(".deleteBtn");
+    for (const element of elements) {
+      element.addEventListener("click", function (event) {
+        event.target.parentNode.parentNode.style.display = "none";
+      });
+    }
+
+    // done
+
+    const doneBtn = document.querySelectorAll(".doneBtn");
+    for (const element of doneBtn) {
+      element.addEventListener("click", function (event) {
+        event.target.parentNode.parentNode.style.textDecoration = "underline";
+        event.target.parentNode.parentNode.style.backgroundColor = "blue";
+      });
+    }
+    // ClearAll
+    const clearBtn = document.getElementById("clear-btn");
+    clearBtn.addEventListener("click", function (event) {
+      event.target.parentNode.style.display = "none";
+    });
+  }
+});
